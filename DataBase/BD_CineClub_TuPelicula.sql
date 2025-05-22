754376bf8f8e32a9698e03521ee52472b67fac81
Kevin Montaño Restrepo <kevinmtrpo98@gmail.com>
2025-05-22 14:21:21 -0500

    Update BD_CineClub_TuPelicula.sql
    CORRECCIONES BD

--- DataBase/BD_CineClub_TuPelicula.sql
+++ DataBase/BD_CineClub_TuPelicula.sql
@@ -21,10 +21,28 @@ CREATE TABLE cargo (
     descripcion NVARCHAR(100) NOT NULL
 );
 
+-- 15. EMPLEADO
+CREATE TABLE empleado (
+    id_empleado INT PRIMARY KEY IDENTITY(1,1),
+    FK_id_tipo_documento INT NOT NULL,
+	FK_id_empleado INT,
+    numero_documento VARCHAR(20) NOT NULL UNIQUE,
+    nombre NVARCHAR(100) NOT NULL,
+	apellido1 NVARCHAR(100),
+	apellido2 NVARCHAR(100),
+	activo BIT DEFAULT 0,
+    FK_id_cargo INT NOT NULL,
+    fecha_ingreso DATE,
+    FOREIGN KEY (FK_id_tipo_documento) REFERENCES tipo_documento(id_tipo_documento),
+    FOREIGN KEY (FK_id_cargo) REFERENCES cargo(id_cargo),
+	FOREIGN KEY (FK_id_empleado) REFERENCES empleado(id_empleado)
+);
+
 -- 3. ESTADO_CONSERVACION
 CREATE TABLE estado_conservacion (
     id_estado_conservacion INT PRIMARY KEY IDENTITY(1,1),
-    descripcion NVARCHAR(100) NOT NULL
+    descripcion NVARCHAR(100) NOT NULL,
+	activo BIT DEFAULT 0
 );
 
 -- 4. NACIONALIDAD
@@ -38,21 +56,33 @@ CREATE TABLE nacionalidad (
 -- 5. DIRECTOR
 CREATE TABLE director (
     id_director INT PRIMARY KEY IDENTITY(1,1),
+	FK_id_empleado INT,
     nombre NVARCHAR(100) NOT NULL,
+	activo BIT DEFAULT 0,
     FK_id_nacionalidad INT,
-    FOREIGN KEY (FK_id_nacionalidad) REFERENCES nacionalidad(id_nacionalidad)
+    FOREIGN KEY (FK_id_nacionalidad) REFERENCES nacionalidad(id_nacionalidad),
+	FOREIGN KEY (FK_id_empleado) REFERENCES empleado(id_empleado)
 );
 
 -- 6. PELICULA
 CREATE TABLE pelicula (
     id_pelicula INT PRIMARY KEY IDENTITY(1,1),
+	FK_id_empleado INT,
+	FK_id_nacionalidad INT,
+	FK_id_director INT NOT NULL,
     titulo NVARCHAR(200) NOT NULL,
-    FK_id_nacionalidad INT,
     productora NVARCHAR(100),
     fecha_estreno DATE,
-    FK_id_director INT NOT NULL,
+	activo BIT DEFAULT 0,    
     FOREIGN KEY (FK_id_nacionalidad) REFERENCES nacionalidad(id_nacionalidad),
-    FOREIGN KEY (FK_id_director) REFERENCES director(id_director)
+    FOREIGN KEY (FK_id_director) REFERENCES director(id_director),
+	FOREIGN KEY (FK_id_empleado) REFERENCES empleado(id_empleado)
+);
+-- GENERO
+CREATE TABLE genero (
+	id_genero INT PRIMARY KEY,
+	nom_genero NVARCHAR(100),
+	activo BIT DEFAULT 0,
 );
 
 -- 7. ACTOR
@@ -60,8 +90,17 @@ CREATE TABLE actor (
     id_actor INT PRIMARY KEY IDENTITY(1,1),
     nombre NVARCHAR(100) NOT NULL,
     FK_id_nacionalidad INT,
+	FK_id_genero INT,
     sexo CHAR(1) CHECK (sexo IN ('M', 'F')),
-    FOREIGN KEY (FK_id_nacionalidad) REFERENCES nacionalidad(id_nacionalidad)
+	activo BIT DEFAULT 0,
+    FOREIGN KEY (FK_id_nacionalidad) REFERENCES nacionalidad(id_nacionalidad),
+    FOREIGN KEY (FK_id_genero) REFERENCES genero(id_genero),
+);
+
+-- PAPEL 
+CREATE TABLE papel (
+	id_papel INT PRIMARY KEY,
+	nom_papel NVARCHAR(100),
 );
 
 -- 8. PELICULA_ACTOR
@@ -69,7 +108,8 @@ CREATE TABLE pelicula_actor (
 	id_pelicula_actor INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
     FK_id_pelicula INT,
     FK_id_actor INT,
-    es_principal BIT DEFAULT 0,
+    nombre_per NVARCHAR(100),
+	avtivo BIT DEFAULT 0,
     FOREIGN KEY (FK_id_pelicula) REFERENCES pelicula(id_pelicula),
     FOREIGN KEY (FK_id_actor) REFERENCES actor(id_actor)
 );
@@ -78,77 +118,83 @@ CREATE TABLE pelicula_actor (
 CREATE TABLE ejemplar (
     id_ejemplar INT PRIMARY KEY IDENTITY(1,1),
     FK_id_pelicula INT NOT NULL,
+	FK_id_empleado INT,
     numero_ejemplar INT NOT NULL,
+	activo BIT DEFAULT 0,
     FK_id_estado_conservacion INT,
     FOREIGN KEY (FK_id_pelicula) REFERENCES pelicula(id_pelicula),
-    FOREIGN KEY (FK_id_estado_conservacion) REFERENCES estado_conservacion(id_estado_conservacion)
+    FOREIGN KEY (FK_id_estado_conservacion) REFERENCES estado_conservacion(id_estado_conservacion),
+	FOREIGN KEY (FK_id_empleado) REFERENCES empleado(id_empleado)
 );
 
 -- 10. CLIENTE
 CREATE TABLE cliente (
-    nroDocCli VARCHAR(20) PRIMARY KEY,
+	id_cliente INT PRIMARY KEY,
+    nroDocCli VARCHAR(20) NOT NULL,
     nombre NVARCHAR(100) NOT NULL,
     FK_id_tipo_documento INT NOT NULL,
-    FOREIGN KEY (FK_id_tipo_documento) REFERENCES tipo_documento(id_tipo_documento)
+	FK_id_cliente INT,
+    FOREIGN KEY (FK_id_tipo_documento) REFERENCES tipo_documento(id_tipo_documento),
+    FOREIGN KEY (FK_id_cliente) REFERENCES cliente(id_cliente),
+);
+
+-- CIUDAD
+
+CREATE TABLE ciudad (
+	id_ciudad INT PRIMARY KEY IDENTITY(1,1),
+	nom_ciudad NVARCHAR(100),
+	cod_postal NVARCHAR(100),
 );
 
 -- 11. DIRECCION_CLIENTE
 CREATE TABLE direccion_cliente (
     id_direccion INT PRIMARY KEY IDENTITY(1,1),
-    FK_nroDocCli VARCHAR(20) NOT NULL,
+    FK_id_cliente INT,
+    FK_id_ciudad INT,
     direccion NVARCHAR(200),
     ciudad NVARCHAR(100),
-    FOREIGN KEY (FK_nroDocCli) REFERENCES cliente(nroDocCli)
+    FOREIGN KEY (FK_id_cliente) REFERENCES cliente(id_cliente),
+    FOREIGN KEY (FK_id_ciudad) REFERENCES ciudad(id_ciudad),
+);
+-- TIPO DE CELULAR
+CREATE TABLE tipo_telefono (
+	id_tip_telefono INT PRIMARY KEY,
+	nom_tip_telefono NVARCHAR(100),
 );
 
 -- 12. TELEFONO_CLIENTE
 CREATE TABLE telefono_cliente (
     id_telefono INT PRIMARY KEY IDENTITY(1,1),
-    FK_nroDocCli VARCHAR(20) NOT NULL,
+    FK_id_cliente INT,
+	FK_id_tip_telefono INT,
     telefono VARCHAR(20),
-    FOREIGN KEY (FK_nroDocCli) REFERENCES cliente(nroDocCli)
-);
-
--- 13. AVAL
-CREATE TABLE aval (
-    id_aval VARCHAR(20) PRIMARY KEY,
-    FK_nroDocCli VARCHAR(20) NOT NULL, -- El cliente que avala
-    nombreAval VARCHAR(100) NOT NULL,
-    FOREIGN KEY (FK_nroDocCli) REFERENCES cliente(nroDocCli)
+    FOREIGN KEY (FK_id_cliente) REFERENCES cliente(id_cliente),
+    FOREIGN KEY (FK_id_tip_telefono) REFERENCES tipo_telefono(id_tip_telefono),
 );
 
 -- 14. USUARIO_SISTEMA
 CREATE TABLE usuario_sistema (
     id_usuario INT PRIMARY KEY IDENTITY(1,1),
+	FK_id_empleado INT,
     nombre_usuario NVARCHAR(50) NOT NULL,
-    clave NVARCHAR(50) NOT NULL,
-    contraseña NVARCHAR(100) NOT NULL
+    contraseña NVARCHAR(100) NOT NULL,
+	FOREIGN KEY (FK_id_empleado) REFERENCES empleado(id_empleado)
 );
 
--- 15. EMPLEADO
-CREATE TABLE empleado (
-    id_empleado INT PRIMARY KEY IDENTITY(1,1),
-    FK_id_tipo_documento INT NOT NULL,
-    numero_documento VARCHAR(20) NOT NULL UNIQUE,
-    nombre NVARCHAR(100) NOT NULL,
-    FK_id_cargo INT NOT NULL,
-    fecha_ingreso DATE,
-    FOREIGN KEY (FK_id_tipo_documento) REFERENCES tipo_documento(id_tipo_documento),
-    FOREIGN KEY (FK_id_cargo) REFERENCES cargo(id_cargo)
-);
+
 
 -- 16. ALQUILER
 CREATE TABLE alquiler (
     id_alquiler INT PRIMARY KEY IDENTITY(1,1),
-    FK_nroDocCli VARCHAR(20) NOT NULL,
-    FK_id_ejemplar INT NOT NULL,
-	FK_id_usuario INT NOT NULL,
+    FK_id_cliente INT,
+    FK_id_ejemplar INT,
+	FK_id_empleado INT,
     fecha_inicio DATE NOT NULL,
     dias_alquiler INT NOT NULL,
     fecha_devolucion DATE,
-    FOREIGN KEY (FK_nroDocCli) REFERENCES cliente(nroDocCli),
+    FOREIGN KEY (FK_id_cliente) REFERENCES cliente(id_cliente),
     FOREIGN KEY (FK_id_ejemplar) REFERENCES ejemplar(id_ejemplar),
-    FOREIGN KEY (FK_id_usuario) REFERENCES usuario_sistema(id_usuario)
+	FOREIGN KEY (FK_id_empleado) REFERENCES empleado(id_empleado)
 );
 
 COMMIT;
